0946507dbf6a15c36e45f5ec5fdb819e803be711
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-06-14 18:25:58 +0100

    CPP Module 01
    feat(ex05): *
    feat(ex06): *

--- cpp_module_01/ex05/main.cpp
+++ cpp_module_01/ex05/main.cpp
@@ -6,7 +6,24 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:01:51 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/06/13 17:01:51 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:01:08 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl person;
+
+	std::cout << "\n[ DEBUG ]" << std::endl;
+	person.complain("debug");
+	std::cout << "\n[ INFO ]" << std::endl;
+	person.complain("info");
+	std::cout << "\n[ WARNING ]" << std::endl;
+	person.complain("warning");
+	std::cout << "\n[ ERROR ]" << std::endl;
+	person.complain("error");
+	return (0);
+}
