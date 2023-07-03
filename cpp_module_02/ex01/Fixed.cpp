81af2edc973dda4fb15901cb9912549ed8e7d9c2
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-07-03 21:04:22 +0100

    CPP Module 02
    fix(ex01): fix compilation error

--- cpp_module_02/ex01/Fixed.cpp
+++ cpp_module_02/ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:06:07 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/07/03 19:08:43 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/07/03 20:59:56 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <cmath>
 
 Fixed::Fixed()
 {
@@ -21,24 +22,25 @@ Fixed::Fixed()
 Fixed::Fixed(const int value)
 {
 	std::cout << "Int constructor called\n";
-	this->fixedPoint = value;
+	this->fixedPoint = value * (1 << Fixed::fractionalBits);
 }
 
 Fixed::Fixed(const float value)
 {
 	std::cout << "Float constructor called\n";
-	this->fixedPoint = (float)value * (1 << Fixed::fractionalBits);
+	this->fixedPoint = roundf(value * (1 << Fixed::fractionalBits));
 }
 
-Fixed::Fixed(Fixed& value)
+Fixed::Fixed(Fixed const& value)
 {
+	std::cout << "Copy constructor called\n";
 	*this = value;
 }
 
-Fixed& Fixed::operator=(Fixed& value)
+Fixed& Fixed::operator=(const Fixed& value)
 {
 	std::cout << "Copy assignment operator called\n";
-	this->fixedPoint = value.getRawBits();
+	this->setRawBits(value.getRawBits());
 	return (*this);
 }
 
@@ -47,15 +49,13 @@ Fixed::~Fixed()
 	std::cout << "Destructor called\n";
 }
 
-int Fixed::getRawBits(void)
+int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called\n";
 	return (this->fixedPoint);
 }
 
 void Fixed::setRawBits(const int raw)
 {
-	std::cout << "setRawBits member function called\n";
 	this->fixedPoint = raw;
 }
 
@@ -66,7 +66,7 @@ int Fixed::toInt() const
 
 float Fixed::toFloat() const
 {
-	return ((float)this->fixedPoint / (1 << Fixed::fractionalBits));
+	return ((float)this->fixedPoint / (float)(1 << Fixed::fractionalBits));
 }
 
 std::ostream& operator<<(std::ostream& out, Fixed const& value)
--- cpp_module_02/ex01/Fixed.hpp
+++ cpp_module_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:06:15 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/07/03 19:05:25 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/07/03 20:40:15 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ public:
 	Fixed();						// Constructor
 	Fixed(const int value);			// Parameterized Constructor
 	Fixed(const float value);		// Parameterized Constructor
-	Fixed(Fixed& value);		// Copy Constructor
-	Fixed& operator=(Fixed& value);	// Copy Assignment Operator Overload
+	Fixed(const Fixed& value);		// Copy Constructor
+	Fixed& operator=(const Fixed& value);	// Copy Assignment Operator Overload
 	~Fixed();						// Destructor
 	void setRawBits(int const raw);
-	int getRawBits(void);
+	int	getRawBits(void) const;
 	int toInt(void) const;
 	float toFloat(void) const;
 };
