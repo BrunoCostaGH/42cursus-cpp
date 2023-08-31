a67502ab7877f6988a4f7bf4541e4d0192324402
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-08-31 16:09:03 +0100

    CPP Module 07
    fix ex01 not working on template functions with different typename

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,14 +6,14 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:54:42 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/08/23 14:50:39 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/08/31 16:05:03 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename T>
-void iter(T* address, int length, void(*f)(T&))
+template <typename T, typename U>
+void iter(T* address, int length, void(*f)(U&))
 {
-	for (int i = 0; i <= length; i++)
+	for (int i = 0; i < length; i++)
 	{
 		f(address[i]);
 	}
